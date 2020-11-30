b6735a631a7b09606f8ea4cf22f00130314ef2f3
Gregory Belevsky <gregory664@yandex.ru>
2020-11-30 09:17:20 +0300

    add PublisherRepositoryImpl

--- src/main/java/org/library/services/PublisherService.java
+++ src/main/java/org/library/services/PublisherService.java
@@ -1,147 +1,49 @@
 package org.library.services;
 
 import org.library.entity.Publisher;
-import org.library.exceptions.SQLExceptionWrapper;
 import org.library.interfaces.PublisherRepository;
-import org.library.utils.ConnectionUtils;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import static org.library.utils.statements.PublisherSQLStatements.*;
 
 public class PublisherService implements PublisherRepository {
     @Override
     public List<Publisher> findAll() {
-        List<Publisher> publishers = new ArrayList<>();
-
-        try (Connection connection = ConnectionUtils.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_ALL);
-             ResultSet resultSet = statement.executeQuery()) {
-            while (resultSet.next()) {
-                int id = resultSet.getInt(1);
-                String title = resultSet.getString(2);
-                publishers.add(new Publisher(id, title));
-            }
-        } catch (SQLException e) {
-            throw new SQLExceptionWrapper(e);
-        }
-        return publishers;
+        return null;
     }
 
     @Override
     public Optional<Publisher> findById(Integer id) {
-        Publisher publisher = null;
-
-        try (Connection connection = ConnectionUtils.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID)) {
-            statement.setInt(1, id);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    int newId = resultSet.getInt(1);
-                    String title = resultSet.getString(2);
-                    publisher = new Publisher(newId, title);
-                }
-            }
-        } catch (SQLException e) {
-            throw new SQLExceptionWrapper(e);
-        }
-        return Optional.ofNullable(publisher);
+        return Optional.empty();
     }
 
     @Override
     public boolean existsById(Integer id) {
-        boolean isExists = false;
-
-        try (Connection connection = ConnectionUtils.getConnection();
-             PreparedStatement statement = connection.prepareStatement(EXISTS_BY_ID)) {
-            statement.setInt(1, id);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    isExists = resultSet.getInt(1) == 1;
-                }
-            }
-        } catch (SQLException e) {
-            throw new SQLExceptionWrapper(e);
-        }
-        return isExists;
+        return false;
     }
 
     @Override
     public void deleteAll() {
-        try (Connection connection = ConnectionUtils.getConnection();
-             Statement statement = connection.createStatement()) {
-            statement.executeUpdate(DELETE);
-        } catch (SQLException e) {
-            throw new SQLExceptionWrapper(e);
-        }
+
     }
 
     @Override
     public boolean deleteById(Integer id) {
-        boolean result;
-
-        try (Connection connection = ConnectionUtils.getConnection();
-             PreparedStatement statement = connection.prepareStatement(DELETE_BY_ID)) {
-            statement.setInt(1, id);
-            result = statement.executeUpdate() == 1;
-        } catch (SQLException e) {
-            throw new SQLExceptionWrapper(e);
-        }
-        return result;
+        return false;
     }
 
     @Override
     public boolean save(Publisher publisher) {
-        boolean isSave;
-
-        try (Connection connection = ConnectionUtils.getConnection();
-             PreparedStatement statement = connection.prepareStatement(SAVE)) {
-            statement.setString(1, publisher.getTitle());
-            isSave = statement.executeUpdate() == 1;
-
-        } catch (SQLException e) {
-            throw new SQLExceptionWrapper(e);
-        }
-        return isSave;
+        return false;
     }
 
     @Override
     public long count() {
-        long result = 0;
-
-        try (Connection connection = ConnectionUtils.getConnection();
-             PreparedStatement statement = connection.prepareStatement(COUNT)) {
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    result = resultSet.getInt(1);
-                }
-            }
-        } catch (SQLException e) {
-            throw new SQLExceptionWrapper(e);
-        }
-        return result;
+        return 0;
     }
 
     @Override
     public Optional<Publisher> findByTitle(String title) {
-        Publisher publisher = null;
-
-        try (Connection connection = ConnectionUtils.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_BY_TITLE)) {
-            statement.setString(1, title);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("id");
-                    String fTitle = resultSet.getString("title");
-                    publisher = new Publisher(id, fTitle);
-                }
-            }
-        } catch (SQLException e) {
-            throw new SQLExceptionWrapper(e);
-        }
-        return Optional.ofNullable(publisher);
+        return Optional.empty();
     }
 }
